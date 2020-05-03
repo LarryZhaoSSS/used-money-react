1a872284b3bb6e22aef440ba9daea12c57e7cf4a
LarryKameZhao <larryzhao1994@outlook.com>
2020-05-03 16:14:12 +0800

    number pad has finished

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -61,7 +61,15 @@ const Wrapper = styled.section`
   }
 `;
 export const NumberPadSection: React.FC = () => {
-  const [output, setOutput] = useState('0');
+  const [output, _setOutput] = useState('0');
+  const setOutput = (output: string) => {
+    if (output.length > 16) {
+      output = output.slice(0, 16);
+    } else if (output.length === 0) {
+      output = '0';
+    }
+    _setOutput(output);
+  };
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
     if (text === null) {
@@ -78,20 +86,31 @@ export const NumberPadSection: React.FC = () => {
       case '7':
       case '8':
       case '9':
-      case '.':
         console.log(text);
         if (output === '0') {
           setOutput(text);
         } else {
           setOutput(output + text);
         }
-      
+
+        break;
+      case '.':
+        if (output.includes('.')) {
+          return;
+        }
+        setOutput(output + '.');
         break;
       case '删除':
+        if (output.length === 1) {
+          setOutput('0');
+        } else {
+          setOutput(output.slice(0, -1));
+        }
         console.log('删除');
         break;
       case '清空':
         console.log('清空');
+        setOutput('');
         break;
       case 'ok':
         console.log('ok');
