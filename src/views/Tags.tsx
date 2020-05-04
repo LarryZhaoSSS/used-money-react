2e58d17b19ef138565c191bcc23cc5fddc5a9dc4
LarryKameZhao <larryzhao1994@outlook.com>
2020-05-04 15:25:36 +0800

    tags style

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,16 +1,59 @@
 import React from 'react';
 import { Layout } from 'components/Layout';
 import { useTags } from 'useTags';
+import styled from 'styled-components';
+import { Icon } from 'components/Icon';
 
+const TagList = styled.ol`
+  font-size: 16px;
+  background-color: white;
+  > li {
+    border-bottom: 1px solid #d5d5d9;
+    line-height: 20px;
+    padding: 12px 16px 12px 0;
+    margin-left: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 10px 12px;
+  background-color: #767676;
+  color: white;
+  border-radius: 4px;
+`;
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+const Space = styled.div`
+  height: 16px;
+`;
 export const Tags = () => {
   const { tags, setTags } = useTags();
   return (
     <Layout>
-      <ol>
+      <TagList>
         {tags.map((tag) => {
-          return <li key={tag}>{tag}</li>;
+          return (
+            <li key={tag}>
+              <span className='oneLine'> {tag}</span>
+              <Icon name='arrow-right' />
+            </li>
+          );
         })}
-      </ol>
+      </TagList>
+      <Center>
+        <Space />
+        <Space />
+        <Space />
+        <Button>新增标签</Button>
+      </Center>
     </Layout>
   );
 };
